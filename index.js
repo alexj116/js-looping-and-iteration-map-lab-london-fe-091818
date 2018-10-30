596a111cfe23a03b851f0df3dd3f0658bc6e2d8c
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 19:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+const drivers = [];
+
+function lowerCaseDrivers (drivers) {
+   return drivers.map(d =>d.toLowerCase())
+}
+
+
+function nameToAttributes (drivers) {
+  return drivers.map(d => {
+    let newArray = d.split(" ");
+    return {firstName: newArray[0], lastName: newArray[1]};
+  })
+}
+
+// correct upto here
+
+function attributesToPhrase (drivers)
